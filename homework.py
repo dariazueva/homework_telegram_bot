635ee059a8d2766f0bc2997b95c940bf5b896479
Дарья Зуева <DZueva26@gmail.com>
2023-08-20 19:00:55 +0500

    Third

--- homework.py
+++ homework.py
@@ -1,12 +1,21 @@
 import os
+import logging
 import telegram
 from dotenv import load_dotenv
 import time
 import requests
+from logging import StreamHandler
 
 
 load_dotenv()
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = StreamHandler()
+formatter=logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -29,31 +38,41 @@ def check_tokens():
     try:
         if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
             raise ValueError('Отсуствуют переменные окружения.')
-    except ValueError:
+    except ValueError as error:
+        logger.critical(error)
         exit()
 
 def send_message(bot, message):
     """Отправка сообщения в Telegram чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.debug('Удачная отправка сообщения в Telegram.')
     except telegram.TelegramError as error:
-        raise telegram.TelegramError(error)
+        logger.error('Ошибка')
     return message
 
 def get_api_answer(timestamp):
     """Создание запроса к эндпоинту."""
-    response = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
-    if response.status_code != 200:
-        raise requests.RequestException('Полученный статус ответа отличается от 200.')
-    response = response.json()
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+        if response.status_code != 200:
+            logger.error('Ошибка')
+            raise requests.RequestException('Полученный статус ответа отличается от 200.')
+        response = response.json()
+    except Exception as error:
+        logger.error(error)
+        raise Exception(error)
     return response
 
 def check_response(response):
     """Проверка ответа API."""
     if type(response) != dict:
+        logger.error('Ошибка')
         raise TypeError('Тип данных {type(response)} не соответсвует ожидаемому типу dict.')
     elif ('homeworks' in response) and ('current-time in response'):
+        logger.error('Ошибка')
         if type(response['homeworks']) != list:
+            logger.error('Ошибка')
             raise TypeError('Тип данных {type("homeworks")} не соответсвует ожидаемому типу list.')
         return response.get('homeworks')
     raise KeyError('Подходящего ключа нет в ответе.')
@@ -61,8 +80,10 @@ def check_response(response):
 def parse_status(homework):
     """Извлечение информации из ответа API."""
     if homework['status'] not in HOMEWORK_VERDICTS:
+        logger.error('Ошибка')
         raise KeyError('Статус не найден.')
     elif 'homework_name' not in homework:
+        logger.error('Ошибка')
         raise KeyError('Ключ "homework_name" не найден.')
     homework_name = homework['homework_name']
     verdict = HOMEWORK_VERDICTS[homework['status']]
